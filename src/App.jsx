92a2929637b856e771fe5f4d1abed402bfa98198
ashishthakur113 <ashthakur113@gmail.com>
2026-03-10 23:43:47 +0530

    fix case sensitive import path for Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
-import Home from './Pages/home/Home'
+import Home from './Pages/Home/Home'
 import Cart from './Pages/Cart/Cart'
 import PlaceOrder from './Pages/PlaceOrder/PlaceOrder'
 import Footer from './Components/Footer/Footer'
